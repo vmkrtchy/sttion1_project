71b1ced4565d80b855da16b3f5e73b4d4e88a8ed
vmkrtchy <you@example.com>
2022-11-30 22:17:05 +0400

    finished all story f string

--- main.py
+++ main.py
@@ -4,48 +4,47 @@ def temp3():
     ProperNoun         = input("enter Proper Noun")
     PersonsName        = input("enter Persons Name")
     Noun               = input("enter Noun")
-    Adjective          = input(" enter Adjective")
-    Verb               = input("")
-    Adjective2         = input("")
-    Animal             = input("")
-    Verb2              = input("")
-    Color              = input("")
-    Verbendingining    = input("")
-    Adverbeendinginly  = input("")
-    Number             = input("")
-    MeasureofTime      = input("")
-    Color2             = input("")
-    Animal2            = input("")
-    Number2            = input("")
-    SillyWord          = input("")
-    Noun2              = input("")
-
+    Adjective          = input("enter Adjective")
+    Verb               = input("Enter Verb")
+    Adjective2         = input("Enter Adjective")
+    Animal             = input("Enter Animal")
+    Verb2              = input("Enter verb (-ing)")
+    Color              = input("Enter color")
+    Verbendingining    = input("Enter verb (-ing)")
+    Adverbeendinginly  = input("Enter Adverb (-ing)")
+    Number             = input("Enter Number")
+    MeasureofTime      = input("Enter Measure of Time")
+    Color2             = input("Enter color")
+    Animal2            = input("Enter color")
+    Number2            = input("Enter number")
+    SillyWord          = input("Enter number")
+    Noun2              = input("Enter Noun")
     
-
     temp_3= f"This weekend I am going camping with {ProperNoun} {PersonsName}. I packed my lantern, sleeping bag, and {Noun}. I am so {Adjective} to {Verb} in a tent. I am {Adjective2} we might see an {Animal}, I hear theyre kind of dangerous. While we are camping, we are going to hike, fish, and {Verb2}. I have heard that the {Color} lake is great for { Verbendingining} . Then we will {Adverbeendinginly} hike through the forest for {Number} {MeasureofTime}. If I see a {Color2} {Animal2} while hiking, I am going to bring it home as a pet! At night we will tell {Number2} {SillyWord} stories and roast {Noun2} around the campfire!!"
 
+    print(temp_3)
 
 def temp2():
-    PersonsName
-    Adjective
-    Color
-    Animal
-    Place
-    Adjective2
-    MagicalCreaturePlural
-    Adjective3
-    MagicalCreaturesPlural2
-    RoominaHouse
-    Noun
-    Noun2
-    NounPlural
-    Adjective4
-    NounPlural4
-    Number
-    Measureoftime
-    Verbendingining
-    Adjective5
-    Noun5
+    PersonsName                 = input("enter Persons Name")
+    Adjective                   = input("enter Adjective")
+    Color                       = input("enter Color")
+    Animal                      = input("enter Animal")
+    Place                       = input("enter Place")
+    Adjective2                  = input("enter Adjective")
+    MagicalCreaturePlural       = input("enter Magical Creature Plural")
+    Adjective3                  = input("enter Adjective")
+    MagicalCreaturesPlural2     = input("enter Magical Creatures Plural")
+    RoominaHouse                = input("enter Room in a House")
+    Noun                        = input("enter Noun")
+    Noun2                       = input("enter Noun")
+    NounPlural                  = input("enter Noun (plural)")
+    Adjective4                  = input("enter Adjective")
+    NounPlural4                 = input("enter Noun (Plural)")
+    Number                      = input("enter Number")
+    Measureoftime               = input("enter Measure of time")
+    Verbendingining             = input("enter Verb ending in -ing")
+    Adjective5                  = input("enter Adjective")
+    Noun5                       = input("enter Noun")
 
 
     temp_2 = f" Dear {PersonsName}, I am writing to you from a {Adjective} castle in an enchanted forest. I found myself here one day after going for a ride on a {Color} {Animal} in {Place}. There are {Adjective2} {MagicalCreaturePlural} and {Adjective3} {MagicalCreaturesPlural2} here! In the {RoominaHouse} there is a pool full of {Noun}. I fall asleep each night on a {Noun2} of {NounPlural3} and dream of {Adjective4} {NounPlural4}. It feels as though I have lived here for {Number} { Measureoftime}. I hope one day you can visit, although the only way to get here now is {Verbendingining} on a {Adjective5} {Noun5}!!"
@@ -79,5 +78,6 @@ choose_text = input("choose story a, b  or  c ")
 if choose_text == "a":
     temp_1()
 if choose_text == "b":
-	temp2()
+	temp3()
+if choose_text == "c"
 
